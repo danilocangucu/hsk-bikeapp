9970eb83741bc90e627d10905fea317fe036c42d
danr0x <danr0x@gmail.com>
2023-04-06 13:33:19 +0200

    src/App.js provisional event listener running getJourneys function

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
 import { getStations } from './Stations.js'
+import { getJourneys } from './Journeys.js'
 
 const stations = document.getElementById('stations')
+const journeys = document.getElementById('journeys')
+
 stations.addEventListener('click', getStations)
+journeys.addEventListener('click', getJourneys)
