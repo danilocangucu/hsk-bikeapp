6d943f766b4b15d4b99ed17b5640a30d189c9f36
danr0x <danr0x@gmail.com>
2023-04-22 19:10:19 +0200

    refactor handlers/stations.go to support fetching single station by ID

--- handlers/stations.go
+++ handlers/stations.go
@@ -10,12 +10,14 @@ import (
 
 func StationsGet(w http.ResponseWriter, r *http.Request) {
 	var err error
+	var stations []db.Station
+	var station db.Station
 
 	params := r.URL.Query()
 	filter := db.StationFilter{}
 
-	if id, isExist := params["id"]; isExist {
-		filter.StationId, err = strconv.Atoi(id[0])
+	if id, idExist := params["id"]; idExist {
+		filter.ID, err = strconv.Atoi(id[0])
 		if err != nil {
 			log.Println("Invalid query parameter:", err)
 			http.Error(w, "Error while getting stations", http.StatusBadRequest)
@@ -23,25 +25,36 @@ func StationsGet(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 
-	if err != nil {
-		log.Println("Invalid station id:", err)
-		http.Error(w, "Error while getting stations", http.StatusBadRequest)
-		return
+	if filter.ID != 0 {
+		station, err = DB.GetSingleStation(filter)
+		if err != nil {
+			log.Println("Error while getting station:", err)
+			http.Error(w, "Error while getting station", http.StatusBadRequest)
+			return
+		}
+	} else {
+		stations, err = DB.GetAllStations()
+		if err != nil {
+			log.Println("Error while getting stations:", err)
+			http.Error(w, "Error while getting stations", http.StatusBadRequest)
+			return
+		}
 	}
 
-	stations, err := DB.GetStations(filter)
-	if err != nil {
-		log.Println("Error while getting stations:", err)
-		http.Error(w, "Error while getting stations", http.StatusBadRequest)
-		return
-	}
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 
-	if err := json.NewEncoder(w).Encode(stations); err != nil {
-		log.Println("Error encoding response:", err)
-		http.Error(w, "Error encoding response", http.StatusInternalServerError)
-		return
+	if filter.ID != 0 {
+		if err := json.NewEncoder(w).Encode(station); err != nil {
+			log.Println("Error encoding response:", err)
+			http.Error(w, "Error encoding response", http.StatusInternalServerError)
+			return
+		}
+	} else {
+		if err := json.NewEncoder(w).Encode(stations); err != nil {
+			log.Println("Error encoding response:", err)
+			http.Error(w, "Error encoding response", http.StatusInternalServerError)
+			return
+		}
 	}
-
 }
